98b5c69114a79c028a33b525464c33cd860dfb99
Mariam <mariam.avetis@outlook.com>
2021-10-28 01:38:01 +0400

    test

--- selenium_task/shop/test_shop.py
+++ selenium_task/shop/test_shop.py
@@ -5,8 +5,9 @@
 # Test N2 
 # Remove it from the basket and make sure the basket is empty
 
-from locators.locators import LocatorsXPath
+from locators import LocatorsXPath
 from selenium.webdriver.common.action_chains import ActionChains
+from time import sleep
 
 
 class Shop:
@@ -21,7 +22,6 @@ class Shop:
         self.remove_item = LocatorsXPath.remove_item
         self.message = LocatorsXPath.message
 
-    def add_cart(self):
         card = self.driver.find_element_by_css_selector(self.card)
         hover = self.driver.find_element_by_css_selector(self.hover)
         hover = ActionChains(self.driver).move_to_element(card).move_to_element(hover)
@@ -36,18 +36,28 @@ class Shop:
         hover_cart = ActionChains(self.driver).move_to_element(cart_hover)
         hover_cart.perform()
 
-
-    def cart_quantity(self, text):
         item_quantity = self.driver.find_element_by_class_name(self.quantity).text
-        item_quantity.send_keys(text)
 
-    
-    def remove_cart(self, text):
         cart_hover = self.driver.find_element_by_css_selector(self.cart_hover)
-        cart_hover.click()
 
         remove_item = self.driver.find_element_by_xpath(self.remove_item)
         remove_item.click()
 
         message = self.driver.find_element_by_css_selector(self.message).text
-        message.send_keys(text)
+
+    def check_cart(self):
+        driver = self.driver
+        cart_quantity = self.item_quantity
+        cart_quantity_out = 1
+        assert cart_quantity == cart_quantity_out, "positive test failed"
+
+        sleep(5)
+        driver.close()
+
+    def remove_item(self):
+        driver = self.driver
+        message_cart = self.message
+        assert message_cart == "Your shopping cart is empty", "test failed"
+
+        sleep(5)
+        driver.close()
